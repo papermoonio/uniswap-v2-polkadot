e0f4cf1f7c513f9aa0b5b8c22ae03bdc3b6de601
sekisamu <22696121+sekisamu@users.noreply.github.com>
2026-02-07 14:19:45 +0800

    update hardhat config

--- hardhat.config.js
+++ hardhat.config.js
@@ -56,23 +56,14 @@ module.exports = {
       gas: "auto",
       gasPrice: "auto"
     },
+    // revm
     local: {
-      // polkavm: true,
-      url: `http://127.0.0.1:8545`,
+      url: 'http://localhost:8545',
       accounts: [
-        process.env.LOCAL_PRIV_KEY ??
-          "0x5fb92d6e98884f76de468fa3f6278f8807c48bebc13595d45af5bdc4da702133",
-        process.env.AH_PRIV_KEY ?? '',
+        '0x5fb92d6e98884f76de468fa3f6278f8807c48bebc13595d45af5bdc4da702133',
+        '0x8075991ce870b93a8870eca0c0f91913d12f47948ca0fd25b49c6fa7cdbeee8b',
       ],
-    },
-    passetHub: {
-      polkavm: true,
-      url: "https://testnet-passet-hub-eth-rpc.polkadot.io",
-      accounts: [
-        process.env.AH_PRIV_KEY ?? '',
-        process.env.LOCAL_PRIV_KEY ??
-          "0x5fb92d6e98884f76de468fa3f6278f8807c48bebc13595d45af5bdc4da702133",
-      ],
-    },
+      chainId: 420420420
+    }
   },
 };
